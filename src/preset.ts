f45c094babab320d8745596af6a854020494acd3
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2023-06-07 12:52:04 +0900

    fix: ignore lint error

--- src/preset.ts
+++ src/preset.ts
@@ -104,6 +104,8 @@ async function useNuxtViteConfig() {
             for (const name in vuePlugins) {
               if (!config.plugins?.some((p) => (p as any)?.name === name)) {
                 const [plugin, key] = vuePlugins[name as keyof typeof vuePlugins];
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
                 config.plugins.push(plugin(config[key]));
               }
             }
